5477b5e8d20d42561eaba82bac6d967a6aa8e5b4
Patrick Knight <pfknight2@gmail.com>
2022-08-12 15:30:34 -0400

    working on the song details.

--- client/src/Pages/SongSearch.js
+++ client/src/Pages/SongSearch.js
@@ -2,18 +2,30 @@ import { useNavigate } from 'react-router-dom'
 import SearchBar from '../Components/SearchBar'
 import SongCard from '../Components/SongCard'
 
-const SongSearch = ({ songSearchFilters, setSongSearchFilters }) => {
+const SongSearch = ({ songSearchFilters, setSongSearchFilters, songList }) => {
   //State
   const navigate = useNavigate()
   //Functions
+  const handleSelect = () => {
+    //may move up a level.
+    alert("Selected a song!")
+  }
+  //
   return (
     <div id='songSearch'>
-      <SearchBar
-        songSearchFilters={songSearchFilters}
-        setSongSearchFilters={setSongSearchFilters}
-      />
       <p>The place to look for songs.</p>
-      <p>Results to dispay here with a map function, as 'songCards'</p>
+      <div id='searchOptions'>
+        <SearchBar
+          songSearchFilters={songSearchFilters}
+          setSongSearchFilters={setSongSearchFilters}
+        />
+      </div>
+      <h4>Search Results: </h4>
+      <div id='songCardHolder'>
+        {songList?.map((song, index) => (
+          <SongCard song={song} handleSelect={() => handleSelect(song)}/>
+        ))}
+      </div>
     </div>
   )
 }
