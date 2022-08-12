44d2b7fd3d17d910e4ad1a5959ec43a7fb12a17b
Tommy Le <LeTran.Tommy@gmail.com>
2022-08-12 14:36:42 -0500

    Added Playlist Details

--- client/src/Pages/PlaylistDetails.js
+++ client/src/Pages/PlaylistDetails.js
@@ -1,10 +1,27 @@
-const PlaylistDetails = () => {
+import { useEffect, useState } from 'react'
+
+const PlaylistDetails = (userPlaylist) => {
   //State
+  const [playlist, setPlaylist] = useState('')
+
+  let { id } = useParams()
+
+  useEffect(() => {
+    let selectedPlaylist = playlist.find((playlist) => playlist._id === id)
+    setPlaylist(selectedPlaylist)
+  }, [userPlaylist, id])
   //Functions
   return (
-    <div id='playlistDetailsPage'>
-      <p>A playlist's detail page.</p>
-      <p>Playlist will display songs as song cards.</p>
+    <div id="playlistDetailsPage">
+      {/* <p>A playlist's detail page.</p>
+      <p>Playlist will display songs as song cards.</p> */}
+      {userPlaylist.map((userPlaylist) => (
+        <div key={userPlaylist.name} onClick={() => showPlaylist(userPlaylist)}>
+          <div className="User-Playlist">
+            <h1>The user's playlist(s) will display here</h1>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
