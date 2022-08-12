74884379ba5861c9d07cf3f4b3e4c3b5f385a0a8
Emily Byrne <embyrne13@gmail.com>
2022-08-12 11:23:54 -0400

    login and signup

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,13 +1,51 @@
 import { useNavigate } from 'react-router-dom'
-
+import { useState } from 'react'
 const Login = () => {
-  //State
+  const [formValues, setFormValues] = useState({ username: '', password: '' })
   const navigate = useNavigate()
-  //Functions
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    navigate('/')
+  }
   return (
-    <div id='loginPage'>
-      <p>The user's Login page.</p>
-      <p>Insert form here.</p>
+    <div id="loginPage">
+      <div className="loginCard">
+        <form className="username" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username </label>
+            <input
+              onChange={handleChange}
+              name="username"
+              type="username"
+              placeholder="username"
+              value={formValues.username}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password </label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              placeholder="password"
+              value={formValues.password}
+              required
+            />
+          </div>
+          <button
+            disabled={!formValues.username || !formValues.password}
+            type="submit"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
