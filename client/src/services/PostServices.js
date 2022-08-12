2a98179e062667b79ef698d95c9ee3f4b73f80b3
Patrick Knight <pfknight2@gmail.com>
2022-08-12 09:46:16 -0400

    added services files for auth

---
+++ client/src/services/PostServices.js
@@ -0,0 +1,12 @@
+import Client from './api'
+
+export const GetPosts = async () => {
+  try {
+    const res = await Client.get('/posts')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+// Modify this file as needed to fit our project.
