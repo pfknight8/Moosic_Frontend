49180365698af5ca736ec52d91be6bbb2021b16c
Patrick Knight <pfknight2@gmail.com>
2022-08-15 21:58:20 -0400

    addedd some buttons to details pages

--- client/src/Pages/SongDetails.js
+++ client/src/Pages/SongDetails.js
@@ -1,6 +1,17 @@
-const SongDetails = ({selectedSong}) => {
+import { useNavigate } from "react-router-dom"
+
+const SongDetails = ({selectedSong, setSelectedSong, user, authenticated, songSearchFilters, selectedPlaylist}) => {
   //State
+  const navigate = useNavigate()
   //Functions
+  const handleBackToSearch = () => {
+    setSelectedSong(null)
+    navigate('/songs')
+  }
+  const handleBackToPlaylist = (PL) => {
+    setSelectedSong(null)
+    navigate(`/profile/playlist/${PL.id}`)
+  }
   return (
     <div id='songDetailsPage'>
       <h1>Title: {selectedSong.title}</h1>
@@ -13,6 +24,9 @@ const SongDetails = ({selectedSong}) => {
       </div>
       <p>A selectedSong's detail page.</p>
       <p>Song Details after clicking on a selectedSong card. Is there anything else we should include here? Mafbe check what the 3rd party API will bring in, can expand our model to accomodate it then.</p>
+      {Object.keys(songSearchFilters).length > 0 ? <button onClick={handleBackToSearch}>Back to Search</button> : null}
+      {selectedPlaylist ? <button onClick={() => handleBackToPlaylist(selectedPlaylist)}>Back to Playlist</button> : null}
+      {!selectedPlaylist ? <button>Add to Playlist</button> : null}
     </div>
   )
 }
