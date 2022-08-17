c1df4a2fd3948fe143bd56df99520945dd17688b
Emily Byrne <embyrne13@gmail.com>
2022-08-17 14:12:59 -0400

    structuring website

--- client/src/Pages/SongDetails.js
+++ client/src/Pages/SongDetails.js
@@ -1,5 +1,5 @@
-import { useNavigate } from "react-router-dom"
-import Client, { BASE_URL } from "../services/api"
+import { useNavigate } from 'react-router-dom'
+import Client, { BASE_URL } from '../services/api'
 
 const SongDetails = ({
   selectedSong,
@@ -24,7 +24,9 @@ const SongDetails = ({
   }
   const handleAddToPlaylist = async (playlist_id) => {
     let song_id = selectedSong.id
-    let response = await Client.post(`${BASE_URL}/api/playlist/addsong/${playlist_id}/${song_id}`)
+    let response = await Client.post(
+      `${BASE_URL}/api/playlist/addsong/${playlist_id}/${song_id}`
+    )
     console.log(response.data)
     if (!response.data.message) {
       alert(`song added!`)
@@ -33,44 +35,74 @@ const SongDetails = ({
     }
   }
 
-  let selectOptions;
+  let selectOptions
   if (user && userPlaylists && userPlaylists.length > 0) {
     selectOptions = (
       <div className="optDiv">
-        <h4>Add to a playlist</h4>
+        <h4 className="msg">Add to a playlist</h4>
         {userPlaylists.map((playlist) => (
-          <button key={playlist.id} onClick={() => handleAddToPlaylist(playlist.id)}>{playlist.title}</button>
+          <button
+            className="buttonz"
+            key={playlist.id}
+            onClick={() => handleAddToPlaylist(playlist.id)}
+          >
+            {playlist.title}
+          </button>
         ))}
       </div>
     )
   } else if (!user) {
     selectOptions = (
       <div className="optDiv">
-        <p>You need to sign in!</p>
+        <p className="msg">
+          You need to{' '}
+          <a className="here" href="/userLogin">
+            login!
+          </a>
+        </p>
       </div>
     )
   } else {
     selectOptions = (
       <div className="optDiv">
-        <p>You need to create at least one playlist!</p>
+        <p className="msg">
+          You need to create at least one{' '}
+          <a className="here" href="/profile">
+            playlist!
+          </a>
+        </p>
       </div>
-    )      
+    )
   }
 
   return (
-    <div id='songDetailsPage'>
+    <div id="songDetailsPage">
       <h1>Title: {selectedSong.title}</h1>
       <div className="img-content">
-        {selectedSong.image ? <img className="songCardImg" src={selectedSong.image} alt={selectedSong.title} width="400"/> : null}
+        {selectedSong.image ? (
+          <img
+            className="songCardImg"
+            src={selectedSong.image}
+            alt={selectedSong.title}
+            width="400"
+          />
+        ) : null}
       </div>
       <div className="info-box">
         <h3>Artist: {selectedSong.artist}</h3>
-        <h4>Length: {selectedSong.time} seconds</h4>
+        <h4>Length: {selectedSong.time} minutes</h4>
       </div>
-      <p>A selectedSong's detail page.</p>
-      <p>Song Details after clicking on a selectedSong card. Is there anything else we should include here? Mafbe check what the 3rd party API will bring in, can expand our model to accomodate it then.</p>
-      <button onClick={handleBackToSearch}>Back to Search</button>
-      {selectedPlaylist ? <button onClick={() => handleBackToPlaylist(selectedPlaylist)}>Back to Playlist</button> : null}
+      <button className="buttonz" onClick={handleBackToSearch}>
+        Back to Search
+      </button>
+      {selectedPlaylist ? (
+        <button
+          className="buttonz"
+          onClick={() => handleBackToPlaylist(selectedPlaylist)}
+        >
+          Back to Playlist
+        </button>
+      ) : null}
       {!selectedPlaylist ? selectOptions : null}
     </div>
   )
