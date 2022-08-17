ee9d60dea87742c02f10e5c09ad116d3800efca2
Patrick Knight <pfknight2@gmail.com>
2022-08-17 16:30:57 -0400

    passing LogOut to userControls

--- client/src/App.js
+++ client/src/App.js
@@ -111,6 +111,7 @@ function App() {
                 userPlaylists={userPlaylists}
                 handlePlaylistSelect={handlePlaylistSelect}
                 handleUserPlaylists={handleUserPlaylists}
+                LogOut={LogOut}
               />
             }
           />
