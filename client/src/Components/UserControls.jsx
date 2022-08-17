de71ba23c84b03370438afd4c21138f6172666b0
Patrick Knight <pfknight2@gmail.com>
2022-08-17 16:28:38 -0400

    making the user edit options work

--- client/src/Components/UserControls.jsx
+++ client/src/Components/UserControls.jsx
@@ -1,66 +1,201 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Client, { BASE_URL } from '../services/api'
-import { useParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 
-export default function Edit(props) {
-  const [email, setEmail] = useState(props.user.email)
-  const [username, setUsername] = useState(props.user.username)
-  const [password, setPassword] = useState(props.user.password)
-  let { id } = useParams()
-
-  const updateAccount = async (e) => {
-    const res = await Client.put(`${BASE_URL}/api/user/${props.user.id}`, {
-      username: username,
-      password: password
+export default function Edit({user, LogOut}) {
+  const [newUsername, setNewUsername] = useState(user.username)
+  const [newEmail, setNewEmail] = useState(user.email)
+  const [newPassword, setNewPassword] = useState(null)
+  const [oldPassword, setOldPassword] = useState(null)
+  const [confirmPassword, setConfirmPassword] = useState(null)
+  const [editUsername, toggleEditUsername] = useState(false)
+  const [editEmail, toggleEditEmail] = useState(false)
+  const [editPassword, toggleEditPassword] = useState(false)
+  const [wantDelete, toggleWantDelete] = useState(false)
+  const navigate = useNavigate()
+  
+  const updateUsername = async (e) => {
+    await Client.put(`${BASE_URL}/api/user/username/${user.id}`, {
+      id: user.id,
+      username: newUsername,
+      password: oldPassword
     })
   }
-  const navigate = useNavigate()
 
-  const handleReturn = () => {
-    navigate('/profile')
+  const updateEmail = async (e) => {
+    await Client.put(`${BASE_URL}/api/user/email/${user.id}`, {
+      id: user.id,
+      email: newEmail,
+      password: oldPassword
+    })
   }
-  const deleteAccount = async (user) => {
-    const res = await Client.delete(`${BASE_URL}/api/user/${props.user.id}`)
+
+  const updatePassword = async () => {
+    if (newPassword === confirmPassword) {
+      await Client.put(`${BASE_URL}/api/user/${user.id}`, {
+        id: user.id,
+        newPassword: newPassword,
+        oldPassword: oldPassword
+      })
+    } else {
+      alert('Error"! New password does not match confirm!')
+    }
   }
+
+
+  const handleEditBtn = (e) => {
+    e.preventDefault()
+    switch(e.target.id) {
+      case 'editUsernameBtn':
+        toggleEditUsername(!editUsername)
+        break
+      case 'editEmailBtn':
+        toggleEditEmail(!editEmail)
+        break
+      case 'editPasswordBtn':
+        toggleEditPassword(!editPassword)
+        break
+      default:
+        alert('Something went very wrong!')
+    }
+  }
+
+  const deleteAccount = async () => {
+    const res = await Client.delete(`${BASE_URL}/api/user/${user.id}`, { data: {
+      id: user.id,
+      password: oldPassword
+    }})
+    alert(res.data.message)
+    LogOut()
+    navigate('/')
+  }
+
   return (
-    <div>
-      <div>
-        <form className="edit" onSubmit={updateAccount}>
+    <div className='editInfo'>
+      <div id='usernameUpdateForm'>
+        {editUsername ? (
+          <form className='oneFieldForm'>
+            <label htmlFor='usernameChange'>New username: </label>
+            <input
+              className="edittext"
+              id="usernameChange"
+              type="text"
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+              placeholder={user.username}
+              />
+            <label htmlFor='confirmOldPassword'>Current Password: </label>
+            <input
+              className="edittext"
+              id='confirmOldPassword'
+              type="password"
+              onChange={(e) => setOldPassword(e.target.value)}
+              placeholder="*********"
+              required
+            />
+            <button
+              className='buttonz'
+              id='newUsername'
+              onClick={updateUsername}
+            >
+              Submit
+            </button>
+          </form>
+        ) : <h3>Current username: {user.username}</h3> }
+        <button className='buttonz' id='editUsernameBtn' onClick={handleEditBtn}>{editUsername ? 'Cancel' : 'Edit'}</button>
+      </div>
+      <div id='emailUpdateForm'>
+        { editEmail ? (
+        <form className='oneFieldForm'>
+          <label htmlFor='emailChange'>New email: </label>
           <input
             className="edittext"
+            id="emailChange"
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            name={'email'}
-            placeholder={'email'}
+            value={newEmail}
+            onChange={(e) => setNewEmail(e.target.value)}
+            placeholder={user.email}
           />
+          <label htmlFor='confirmOldPassword'>Current Password: </label>
           <input
             className="edittext"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            name={'username'}
-            placeholder={'username'}
+            id='confirmOldPassword'
+            type="password"
+            onChange={(e) => setOldPassword(e.target.value)}
+            placeholder="*********"
+            required
           />
-          <input
-            className="edittext"
-            type="text"
-            onChange={(e) => setPassword(e.target.value)}
-            name="password"
-            placeholder="password"
-          />
-          <button className="buttonz" type="submit" onClick={handleReturn}>
+          <button
+            className='buttonz'
+            id='newUsername'
+            onClick={updateEmail}
+          >
             Submit
           </button>
+        </form>) : <h4>Current email address: {user.email}</h4> }
+        <button className='buttonz' id='editEmailBtn' onClick={handleEditBtn}>{editEmail ? 'Cancel' : 'Edit'}</button>
+      </div>
+      <div id='passwordUpdateForm'>
+        {editPassword ? (
+        <form className='twoFieldForm'>
+          <label htmlFor='confirmOldPassword'>Current Password: </label>
+          <input
+            className="edittext"
+            id='confirmOldPassword'
+            type="password"
+            onChange={(e) => setOldPassword(e.target.value)}
+            placeholder="*********"
+            required
+            />
+          <label htmlFor='passwordChange'>New Password: </label>
+          <input
+            className="edittext"
+            id='passwordChange'
+            type="password"
+            onChange={(e) => setNewPassword(e.target.value)}
+            placeholder="*********"
+            required
+            />
+          <label htmlFor="confirmPasswordChange">Confirm Password </label>
+          <input
+            className="edittext"
+            id='confirmPasswordChange'
+            type="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="*********"
+            required
+            />
           <button
-            className="buttonZ"
-            onClick={() => deleteAccount(props.user._id)}
-          >
-            Delete Account
+            className="buttonz"
+            type="submit"
+            onClick={updatePassword}
+            disabled={
+              newPassword !== confirmPassword
+            }
+            >
+            Submit
           </button>
         </form>
+        ) : <h5>Change Password?</h5> }
+        <button className='buttonz' id='editPasswordBtn' onClick={handleEditBtn}>{editPassword ? 'Cancel' : 'Edit'}</button>
+      </div>
+      <div id='deleteAccountBtns'>
+        {wantDelete ? (
+          <div id='deleteProtect'>
+            <h4>Are you sure?</h4>
+            <label htmlFor='deleteConfirm'>Enter password to confirm: </label>
+            <input
+              className="edittext"
+              id='confirmOldPassword'
+              type="password"
+              onChange={(e) => setOldPassword(e.target.value)}
+              placeholder="*********"
+              required
+            />
+            <button className='buttonZ' disabled={!oldPassword} onClick={deleteAccount}>Yes, Delete Account</button>
+          </div>) : null}
+          <button className='buttonZ' onClick={() => toggleWantDelete(!wantDelete)}>{wantDelete ? 'No, Keep my Account!' : 'Delete Account'}</button>
       </div>
     </div>
   )
